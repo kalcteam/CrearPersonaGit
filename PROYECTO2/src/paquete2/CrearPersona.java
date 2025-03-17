ea7ccbfedd3bbfd9cb1742cd52122eaa6b5e5aa3
carlo <carlo@DESKTOP-E2NQIEV>
2025-03-17 18:49:49 +0100

    Mostramos en pantalla titulos y lineas

--- PROYECTO2/src/paquete2/CrearPersona.java
+++ PROYECTO2/src/paquete2/CrearPersona.java
@@ -8,6 +8,10 @@ public class CrearPersona {
 		persona1.mostrarInformacion();
 		Persona persona2=new Persona ("Juan", 27);
 		persona2.mostrarInformacion();
+		
+		System.out.println("------------------------");
+		System.out.println("Estudiantes:");
+		System.out.println("............");
 	}
 
 }
